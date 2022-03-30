e6e0cd6170ffe7ffc2acec5810eedc918d42936d
Wing <wingjeeplau@gmail.com>
2022-03-30 15:59:34 -0700

    v2 tag

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const globalVar = process.env;
 const app = express()
 console.log(globalVar.appEvn)
 app.get('/', function (req, res) {
-  res.send('Hello World !!' + globalVar.appEvn)
+  res.send('Hello World !!' + globalVar.appEvn + "tag v2")
 })
 
 app.listen(3003)
